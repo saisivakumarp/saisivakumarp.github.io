c3044f3376cd9e8e8bfbc0c1f74ef51613abbc57
Ramya <ramyaram48@gmail.com>
2020-05-02 16:33:30 -0500

    Updates

---
+++ precache-manifest.71f401ed235ed1d791bb214ef6c48273.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9899323f1634e7090d17fc7944c647",
+    "url": "/saisivakumarp/Profile/index.html"
+  },
+  {
+    "revision": "9bc4c1a1d35afaa3ea65",
+    "url": "/saisivakumarp/Profile/static/css/main.2a5569ca.chunk.css"
+  },
+  {
+    "revision": "8439f46d63d0dbf5b55b",
+    "url": "/saisivakumarp/Profile/static/js/2.baffff82.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/saisivakumarp/Profile/static/js/2.baffff82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bc4c1a1d35afaa3ea65",
+    "url": "/saisivakumarp/Profile/static/js/main.613706f1.chunk.js"
+  },
+  {
+    "revision": "97519a4c1a4e8b742e45",
+    "url": "/saisivakumarp/Profile/static/js/runtime-main.3d0ae8c6.js"
+  },
+  {
+    "revision": "2e850cb4da2159a6374f92bfb038465e",
+    "url": "/saisivakumarp/Profile/static/media/CHEM211L_General_Experimental_Chemistry.2e850cb4.pdf"
+  },
+  {
+    "revision": "1edda5ec0dde126d243d5330ee16f8c5",
+    "url": "/saisivakumarp/Profile/static/media/CHEM211_Assignment_Rubric.1edda5ec.pdf"
+  },
+  {
+    "revision": "e4f6f4fa0a870f0f4ed2bca955b05585",
+    "url": "/saisivakumarp/Profile/static/media/CHEM437WI_Physical_Chemistry.e4f6f4fa.pdf"
+  },
+  {
+    "revision": "596f27cb8e546e774db603b0e011e415",
+    "url": "/saisivakumarp/Profile/static/media/CHEM5530A_Lesson_Plan.596f27cb.pdf"
+  },
+  {
+    "revision": "596f27cb8e546e774db603b0e011e415",
+    "url": "/saisivakumarp/Profile/static/media/CHEM5530A_Physical_Chemistry.596f27cb.pdf"
+  },
+  {
+    "revision": "e5b003693ebbca7838febc9e193ded90",
+    "url": "/saisivakumarp/Profile/static/media/CHEM5533_Quantum_Chemistry.e5b00369.pdf"
+  },
+  {
+    "revision": "dc615ab2f2e7af1b54a073dc103da72f",
+    "url": "/saisivakumarp/Profile/static/media/Image1.dc615ab2.png"
+  },
+  {
+    "revision": "89afa999811cb32855632bbcca700b6d",
+    "url": "/saisivakumarp/Profile/static/media/Image10.89afa999.jpg"
+  },
+  {
+    "revision": "d9b93862f2f27dae69462318c41c11bb",
+    "url": "/saisivakumarp/Profile/static/media/Image11.d9b93862.jpg"
+  },
+  {
+    "revision": "95f0e6a58bb93a435c18fd09b881a21b",
+    "url": "/saisivakumarp/Profile/static/media/Image12.95f0e6a5.jpg"
+  },
+  {
+    "revision": "ee5101575f8d9e58797ee1b00c794abe",
+    "url": "/saisivakumarp/Profile/static/media/Image3.ee510157.png"
+  },
+  {
+    "revision": "810317d1d0277ccf9a2b9e1ada8220f9",
+    "url": "/saisivakumarp/Profile/static/media/Image4.810317d1.jpg"
+  },
+  {
+    "revision": "086327ef9c01545a62a51767c0ab59cb",
+    "url": "/saisivakumarp/Profile/static/media/Image5.086327ef.jpg"
+  },
+  {
+    "revision": "eb9942a6651431c4d92bc67cac0252a1",
+    "url": "/saisivakumarp/Profile/static/media/Image6.eb9942a6.jpeg"
+  },
+  {
+    "revision": "016136788f0babbdf309f0909c6ca82c",
+    "url": "/saisivakumarp/Profile/static/media/Image7.01613678.jpg"
+  },
+  {
+    "revision": "49d7f75f1ea7a42fcee77f20a95ffa57",
+    "url": "/saisivakumarp/Profile/static/media/Image8.49d7f75f.jpg"
+  },
+  {
+    "revision": "12db463b48da6513870370ade977c69b",
+    "url": "/saisivakumarp/Profile/static/media/Image9.12db463b.jpg"
+  },
+  {
+    "revision": "3c87f51701d207595f4352353652a4d9",
+    "url": "/saisivakumarp/Profile/static/media/Sai_Diversity_Statement_Updated.3c87f517.pdf"
+  },
+  {
+    "revision": "b588a06516521e81d0b2998763ad1a86",
+    "url": "/saisivakumarp/Profile/static/media/Sai_Pinnepalli_CV.b588a065.pdf"
+  },
+  {
+    "revision": "77942e3aa309f39ba966814fbc9a1393",
+    "url": "/saisivakumarp/Profile/static/media/TeachingPhilosophyStatement.77942e3a.pdf"
+  },
+  {
+    "revision": "51372342156f40010c110de23632330a",
+    "url": "/saisivakumarp/Profile/static/media/close.51372342.png"
+  },
+  {
+    "revision": "23129101723ff67789eecf683bb73d50",
+    "url": "/saisivakumarp/Profile/static/media/profilepic.23129101.jpg"
+  }
+]);
